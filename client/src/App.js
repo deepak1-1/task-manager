52b039a086f668fcf5b6d74dbe1de90f99a05c9d
Deepak <deepaktewatia2000@gmail.com>
2024-07-22 16:31:00 +0530

    Initial Setup

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+const App = () => {
+    return <div>Initial Setupt</div>
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.PORT || '4000'
+const app = express()
+
+const allowedOrigins = ['http://localhost:3000'] // to restrict user of different origins to send request
+
+app.use('/server/static', express.static('public'))
+app.use(
+    cors({
+        origin: allowedOrigins,
+        credentials: true,
+    })
+)
+app.use(cookieParser())
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(`${req.method} - ${req.url}`)
+    next()
+}) // for check in dev environment
+
+app.post('/test', (req, res) => {
+    console.log()
+    res.status(200).json({ success: true, message: 'Test Successful' })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT: ${PORT}`)
+})
