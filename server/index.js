74c20cebc942658a8daf703e72926f97d525078f
Deepak <deepaktewatia2000@gmail.com>
2024-07-24 21:21:08 +0530

    tweak: production ready code

--- server/index.js
+++ server/index.js
@@ -12,22 +12,17 @@ dotenv.config()
 const PORT = process.env.PORT || '4000'
 const app = express()
 
-const allowedOrigins = ['http://localhost:3000'] // to restrict user of different origins to send request
+const allowedOrigins = ['http://localhost:3000', 'http://localhost'] // to restrict user of different origins to send request
 
 app.use('/server/static', express.static('public'))
 app.use(
     cors({
-        origin: allowedOrigins,
+        origin: '*',
         credentials: true,
     })
 )
 app.use(cookieParser())
 app.use(express.json())
-
-app.use((req, res, next) => {
-    console.log(`${req.method} - ${req.url}`)
-    next()
-}) // for check in dev environment
 
 app.post('/test', (req, res) => {
     res.status(200).json({ success: true, message: 'Test Successful' })
